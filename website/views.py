f1cecb0c2792ce5c647138b131b1cfa72f2d7fc5
pereruannabaala <pereruannabaala@gmail.com>
2024-04-26 22:14:10 +0300

    arrange files

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return 'This is the home page'
+    return render_template('index.html')
