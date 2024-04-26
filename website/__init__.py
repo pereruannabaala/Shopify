42534400e8fa74b8b8090e8920afd668b8959475
pereruannabaala <pereruannabaala@gmail.com>
2024-04-26 21:01:10 +0300

    add routes url

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-from flask import flask
+from flask import Flask
 
 
 
@@ -6,5 +6,14 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hbnwdvbn ajnbsjn ahe'
 
+    from .views import views
+    from .auth import auth
+    from .admin import admin
+
+    app.register_blueprint(views, url_prefix='/') # localhost:5000/about-us
+    app.register_blueprint(auth, url_prefix='/') # localhost:5000/auth/change-password
+    app.register_blueprint(admin, url_prefix='/')
+
+     
     return app
 
