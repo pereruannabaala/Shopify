89c0a5ead7c071319358d211c51dc3c2e7261514
pereruannabaala <pereruannabaala@gmail.com>
2024-04-26 20:51:38 +0300

    add login and signup routes

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,11 @@
 from flask import Blueprint
 
-views = Blueprint('views',__name__)
+auth = Blueprint('auth',__name__)
+
+@auth.route('/login')
+def login():
+    return 'This is the login page'
+
+@auth.route('/sign-up')
+def sign_up():
+    return 'This is the sign up page'
