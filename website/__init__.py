338e30ff11f42c9cb847c3a99b1ad0560be8d785
pereruannabaala <pereruannabaala@gmail.com>
2024-04-28 11:26:11 +0300

    db=sqlalchemy

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
+db = SQLAlchemy
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hbnwdvbn ajnbsjn ahe'
