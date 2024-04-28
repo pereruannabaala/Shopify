004ae3bad8d7711c16e966c02fbd0b3f624410ea
pereruannabaala <pereruannabaala@gmail.com>
2024-04-28 11:32:46 +0300

    create customers table

--- website/models.py
+++ website/models.py
@@ -2,3 +2,10 @@ from . import db
 from flask_login import UserMixin
 from datetime import datetime
 
+class Customer(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(1oo),unique=True)
+    username = db.Column(db.String(100),nullable=False)
+    password_hash= db.Column(db.String(150),nullable=False)
+    date_joined = db.Column(db.DateTime(), default=datetime.utcnow)
+
