3ba0f73e8f20a70c13cc6dca41ff286020de7d2e
pereruannabaala <pereruannabaala@gmail.com>
2024-04-29 14:44:52 +0300

    import flas_forms

---
+++ website/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField, PasswordField, EmailField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, length, NumberRange
+           
