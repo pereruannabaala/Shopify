ff545e7cc46253cab39c1b83fcfa13eb4259fa34
pereruannabaala <pereruannabaala@gmail.com>
2024-04-29 15:22:43 +0300

    import Login fom, signup form and render_template

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from .forms import LoginForm, SignUpForm
 
-auth = Blueprint('auth',__name__)
 
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/sign-up')
+def sign_up(): 
+    return 'This is the sign up page'
+
+
+    
 @auth.route('/login')
 def login():
     return 'This is the login page'
 
-@auth.route('/sign-up')
-def sign_up():
-    return 'This is the sign up page'
