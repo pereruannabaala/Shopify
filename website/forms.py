782fab4e2226d3cbef13339ae52f720067a508d6
pereruannabaala <pereruannabaala@gmail.com>
2024-04-29 15:09:58 +0300

    create SignUp Forms

--- website/forms.py
+++ website/forms.py
@@ -1,4 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, FloatField, PasswordField, EmailField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, length, NumberRange
-           
+    
+class SignUpForm(FlaskForm):
+    email = EmailField('Enter Your Email', validators=[DataRequired()])
+    username = StringField('Enter Your Username', validators=[DataRequired(), length(min=2)])
+    password1 = PasswordField('Enter Your Password', validators=[DataRequired(), length(min=6)])
+    password2 = PasswordField('Confirm Your Password', validators=[DataRequired(), length(min=6)])
+    submit = SubmitField('Sign Up')
+
