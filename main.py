edb645881f6e942783b1b36d92dd2697ad53be23
pereruannabaala <pereruannabaala@gmail.com>
2024-04-30 15:30:45 +0300

    add login form and signup form

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+     app.run(debug=True)
+     
--- website/auth.py
+++ website/auth.py
@@ -5,15 +5,17 @@ from .forms import LoginForm, SignUpForm
 auth = Blueprint('auth', __name__)
 
 
+
 @auth.route('/sign-up')
 def sign_up(): 
     form = SignUpForm()
 
-    return render_template('sign-up.html', form=form)
+    return render_template('website\templates\signup.html', form=form)
 
 
 
 @auth.route('/login')
 def login():
-    return 'This is the login page'
+    form = LoginForm()
+    return render_template('website\templates\login.html', form=form)
 
