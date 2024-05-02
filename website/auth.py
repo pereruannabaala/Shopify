ac4b41e29b310fe8441c73bf53bb21b46f15a6e2
pereruannabaala <pereruannabaala@gmail.com>
2024-05-02 11:48:26 +0300

    import flash and direct

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect
 from .forms import LoginForm, SignUpForm
 from .models import Customer
 from . import db
