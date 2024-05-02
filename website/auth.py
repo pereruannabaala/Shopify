fb5d9d2b4e79574f0d39013a160b55ae4b8b0a21
pereruannabaala <pereruannabaala@gmail.com>
2024-05-02 11:39:59 +0300

    add form validation on submit

--- website/auth.py
+++ website/auth.py
@@ -9,7 +9,14 @@ auth = Blueprint('auth', __name__)
 @auth.route('/signup', methods='GET','POST')
 def sign_up(): 
     form = SignUpForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        username = form.username.data
+        password1 = form.password1.data
+        password2 = form.password2.data
 
+        if password1 == password2:
+            
     return render_template('signup.html', form=form)
 
 
