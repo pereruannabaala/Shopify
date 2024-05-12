20a184e42223c1ebb35562111bac9bdea18f44f2
pereruannabaala <pereruannabaala@gmail.com>
2024-05-12 16:40:53 +0300

    create Shop items Form button

--- website/forms.py
+++ website/forms.py
@@ -22,8 +22,12 @@ class PasswordChangeForm(FlaskForm):
     change_password = SubmitField('Change Password')
 
 class ShopItemsForm(FlaskForm):
-    product_name = StringField('Product Name', validators=[DataRequired(), Length(min=2)])
-    current_price = FloatField('Current Price', validators=[DataRequired(), NumberRange(min=0)])
-    previous_price = FloatField('Previous Price', validators=[DataRequired(), NumberRange(min=0)])
-    in_stock = IntegerField('In Stock', validators=[DataRequired(), NumberRange(min=0)])
+    product_name = StringField('Product Name', validators=[DataRequired()])
+    current_price = FloatField('Current Price', validators=[DataRequired()])
+    previous_price = FloatField('Previous Price', validators=[DataRequired()])
+    in_stock = IntegerField('In Stock', validators=[DataRequired()])
+    product_picture = FileField('Product Picture', validators=[FileRequired()])
+    flash_sale = BooleanField('Flash Sale')
 
+    add_product = SubmitField('Add Product')
+    update_product = SubmitField('Update')
