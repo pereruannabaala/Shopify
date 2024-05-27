faf823eeada1bad96753d503c1d71c1ca1660ee2
pereruannabaala <pereruannabaala@gmail.com>
2024-05-27 18:58:49 +0300

    add secure_filename

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template 
 from flask_login import login_required, current_user
 from .forms import ShopItemsForm
+from werzeug.utils import secure_filename
+
 
 admin = Blueprint('admin',__name__)
 
