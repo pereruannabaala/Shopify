e87f53566031d77f8099648c375646c2fa942855
pereruannabaala <pereruannabaala@gmail.com>
2024-05-27 18:57:37 +0300

    add capture POST request on add shop items

--- website/admin.py
+++ website/admin.py
@@ -10,6 +10,17 @@ admin = Blueprint('admin',__name__)
 def add_shop_items():
     if current_user.id == 1:
         form = ShopItemsForm()
+        if form.validate_on_submit():
+            product_name = form.product_name.data 
+            current_price = form.current_price.data
+            previous_price = form.previous_price.data
+            in_stock = form.in_stock.data
+            flash_sale = form.flash_sale.data
+
+            file = form.product_picture.data
+            file_name = secure_filename(file.filename)
+
+
         return render_template('add-shop-items.html',form=form)
     return render_template('404.html')
 
