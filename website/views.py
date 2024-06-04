160a5d9c1974b7e04266494417cc0d7673c5e1e6
pereruannabaala <pereruannabaala@gmail.com>
2024-06-04 09:29:42 +0300

    generate items dynamically from database

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template
+from .models import Product
+
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+
+    items = Product.query.filter_by(flash_sale=True)
+    return render_template('home.html', items=items)
