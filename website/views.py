2f10b5bfa6e9e35bc09cb9f739a05c8d30b869ff
pereruannabaala <pereruannabaala@gmail.com>
2024-06-16 16:21:54 +0300

    change image path

--- website/views.py
+++ website/views.py
@@ -10,4 +10,9 @@ def home():
     items = Product.query.all()
     for product in items:
         print(len(items),product.product_name, product.current_price, product.previous_price, product.product_picture, product.in_stock)
+    for item in items:
+        if item.product_picture:
+            product_picture_path = item.product_picture.split('/')[1:]
+            final_product_picture_path = '/'.join(product_picture_path)
+            print(final_product_picture_path)
     return render_template('home.html', items=items)
