a3ba21f507c77622e99b6b1972a9d01ca61b33d9
pereruannabaala <pereruannabaala@gmail.com>
2024-07-22 14:24:35 +0300

    uninstall intasend

--- website/test.py
+++ website/test.py
@@ -6,7 +6,7 @@ API_TOKEN = 'ISSecretKey_test_e0561796-274b-458f-b0fa-76bbced5a59d'
 
 service = APIService(token=API_TOKEN, publishable_key=API_PUBLISHABLE_KEY, test=True)
 
-create_order = service.collect.mpesa_stk_push(phone_number=0757364069, email='test@gmai.com', amount=100,
+create_order = service.collect.mpesa_stk_push(phone_number='+254757364069', email='test@gmail.com', amount=100,
                                               narrative='Purchase of Items')
 
 print(create_order)
