d98f206f8be9cc745bfca7d70219562619fb6b04
pereruannabaala <pereruannabaala@gmail.com>
2024-07-22 13:33:20 +0300

    create order

--- website/test.py
+++ website/test.py
@@ -5,3 +5,6 @@ API_PUBLISHABLE_KEY = 'ISPubKey_test_598f1362-004c-4d26-be47-8cee94c488cc'
 API_TOKEN = 'ISSecretKey_test_e0561796-274b-458f-b0fa-76bbced5a59d'
 
 service = APIService(token=API_TOKEN, publishable_key=API_PUBLISHABLE_KEY, test=True)
+
+create_order = service.collect.mpesa_stk_push(phone_number=0757364069, email='test@gmai.com', amount=100,
+                                              narrative='Purchase of Items')
