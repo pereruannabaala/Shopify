40dd98ccbdb1bbbcedb4965ac2e61dec5437bd00
pereruannabaala <pereruannabaala@gmail.com>
2024-08-03 22:44:30 +0300

    add view-orders end point

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from website import create_app
+from website  import db  
+
+from website.models import Customer,Order,Cart,Product
 
 
 app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(
+        Customer=Customer,
+        Order=Order,
+        Cart=Cart,
+        Product=Product,
+        db=db  # Include database in shell context for easy access to models.
+    )
 
 if __name__ == '__main__':
      app.run(debug=True)
