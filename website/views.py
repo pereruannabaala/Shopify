bca2da82c8244c42f48286ce0323862f66def286
pereruannabaala <pereruannabaala@gmail.com>
2024-08-05 23:31:31 +0300

    add all changes

--- website/views.py
+++ website/views.py
@@ -218,5 +218,7 @@ def orders():
 def search():
     if request.method == 'POST':
         search_query = request.form.get('search')
-        items = Product.query.filter_by(Product.product_name.ilike(f'%{search_query}%')).all()
-        return render_template('search.html', items=items)
+        items = Product.query.filter(Product.product_name.ilike(f'%{search_query}%')).all()
+        return render_template('search.html', items=items, cart = Cart.query.filter_by(customer_link=current_user.id).all()
+                            if current_user.is_authenticated else [])
+    return render_template('search.html')
